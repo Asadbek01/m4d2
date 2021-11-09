a983ed33c05739e3d29e3904a44feec8a63b8099
Asadbek Azamjonov <asadbek010azamjonov@gmail.com>
2021-11-09 15:33:00 +0100

    created files

---
+++ src/components/MyFooter.jsx
@@ -0,0 +1,8 @@
+//  import { Carousel } from 'react-bootstrap'
+
+const MyFooter = ()=> {
+    
+}
+
+
+ export default MyFooter;
---
+++ src/components/MyNavbar.jsx
@@ -0,0 +1,18 @@
+import { Navbar, Nav}  from "react-bootstrap";
+// import Navbar from "react-bootstrap/Navbar";
+// import Nav  from "react-bootstrap/Nav";
+const MyNavbar = () => (
+ <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto justify-content-center">
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <Nav.Link href="#deets">More deets</Nav.Link>
+    </Nav>
+     
+  </Navbar.Collapse>
+</Navbar>
+);
+ export default MyNavbar;
