cb4922ef77fbef9ad854bfdc3066053b0a48901a
Asadbek Azamjonov <asadbek010azamjonov@gmail.com>
2021-11-09 17:07:08 +0100

    Update images and definitions.jsx

--- src/components/images and definitions.jsx
+++ src/components/images and definitions.jsx
@@ -1,37 +1,328 @@
-import { Image, Container, Row, Col} from 'react-bootstrap'
-
 const Images = () =>(
-<Container>
-  <Row>
-    <Col xs={3} md={4}>
-      <Image src="https://images-na.ssl-images-amazon.com/images/I/71O3WN6oyzL.jpg" className = "thumbnail w-100 h-75" />
-    <p></p>
-    </Col>
-    <Col xs={3} md={4}>
-      <Image src="https://macmillan-dam.captureweb.co.uk/cdn/macmillan/previews/439664/d2600cec4c0f09bf8e6187a83a066343/0/14665546cf5662d409143d004ffc0c54/131898933.jpg" className = "thumbnail w-100" />
-    <p></p>
-    </Col>
-    <Col xs={3} md={4}>
-      <Image src="https://macmillan-dam.captureweb.co.uk/cdn/macmillan/previews/439664/d2600cec4c0f09bf8e6187a83a066343/0/14665546cf5662d409143d004ffc0c54/131898933.jpg" className = "thumbnail w-100" />
-    <p></p>
-    </Col>
-     <Col xs={3} md={4}>
-      <Image src="https://macmillan-dam.captureweb.co.uk/cdn/macmillan/previews/439664/d2600cec4c0f09bf8e6187a83a066343/0/14665546cf5662d409143d004ffc0c54/131898933.jpg" className = "thumbnail w-100" />
-    <p></p>
-    </Col>
-     <Col xs={3} md={4}>
-      <Image src="https://macmillan-dam.captureweb.co.uk/cdn/macmillan/previews/439664/d2600cec4c0f09bf8e6187a83a066343/0/14665546cf5662d409143d004ffc0c54/131898933.jpg" className = "thumbnail w-100" />
-    <p></p>
-    </Col>
-    <Col xs={3} md={4}>
-      <Image src="https://macmillan-dam.captureweb.co.uk/cdn/macmillan/previews/439664/d2600cec4c0f09bf8e6187a83a066343/0/14665546cf5662d409143d004ffc0c54/131898933.jpg" className = "thumbnail w-100" />
-    <p></p>
-    </Col>
-    <Col xs={3} md={4}>
-      <Image src="https://macmillan-dam.captureweb.co.uk/cdn/macmillan/previews/439664/d2600cec4c0f09bf8e6187a83a066343/0/14665546cf5662d409143d004ffc0c54/131898933.jpg" className = "thumbnail w-100" />
-    <p></p>
-    </Col>
-  </Row>
-</Container>
+    <div class="album py-5 bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+          
+              <img src = "https://images-na.ssl-images-amazon.com/images/I/71O3WN6oyzL.jpg"
+              class="bd-placeholder-img card-img-top"
+              width="100%"
+              height="225" />
+            
+             
+            <div class="card-body">
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <div
+                class="d-flex justify-content-between align-items-center"
+              >
+                <div class="btn-group">
+                  <button
+                    type="button"
+                    class="btn btn-sm btn-outline-secondary"
+                  >
+                    View
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-sm btn-outline-secondary"
+                  >
+                    Edit
+                  </button>
+                </div>
+                <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+               <img src = "https://images-na.ssl-images-amazon.com/images/I/81XDguZCJgL.jpg"
+              class="bd-placeholder-img card-img-top"
+              width="100%"
+              height="225" />
+            
+            <div class="card-body">
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <div
+                class="d-flex justify-content-between align-items-center"
+              >
+                <div class="btn-group">
+                  <button
+                    type="button"
+                    class="btn btn-sm btn-outline-secondary"
+                  >
+                    View
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-sm btn-outline-secondary"
+                  >
+                    Edit
+                  </button>
+                </div>
+                <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+              <img src = "https://images-na.ssl-images-amazon.com/images/I/81coZ37EmxL.jpg"
+              class="bd-placeholder-img card-img-top"
+              width="100%"
+              height="225" />
+            
+            <div class="card-body">
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <div
+                class="d-flex justify-content-between align-items-center"
+              >
+                <div class="btn-group">
+                  <button
+                    type="button"
+                    class="btn btn-sm btn-outline-secondary"
+                  >
+                    View
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-sm btn-outline-secondary"
+                  >
+                    Edit
+                  </button>
+                </div>
+                <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+          <img src = "https://images-na.ssl-images-amazon.com/images/I/81XDguZCJgL.jpg"
+              class="bd-placeholder-img card-img-top"
+              width="100%"
+              height="225" />
+            
+            <div class="card-body">
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <div
+                class="d-flex justify-content-between align-items-center"
+              >
+                <div class="btn-group">
+                  <button
+                    type="button"
+                    class="btn btn-sm btn-outline-secondary"
+                  >
+                    View
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-sm btn-outline-secondary"
+                  >
+                    Edit
+                  </button>
+                </div>
+                <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+              <img src = "https://images-na.ssl-images-amazon.com/images/I/91NZ5Cw-4TL.jpg"
+              class="bd-placeholder-img card-img-top"
+              width="100%"
+              height="225" />
+            
+            <div class="card-body">
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <div
+                class="d-flex justify-content-between align-items-center"
+              >
+                <div class="btn-group">
+                  <button
+                    type="button"
+                    class="btn btn-sm btn-outline-secondary"
+                  >
+                    View
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-sm btn-outline-secondary"
+                  >
+                    Edit
+                  </button>
+                </div>
+                <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+            <img src = "https://images-na.ssl-images-amazon.com/images/I/411nhI-kPYL.jpg"
+              class="bd-placeholder-img card-img-top"
+              width="100%"
+              height="225" />
+            
+            <div class="card-body">
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <div
+                class="d-flex justify-content-between align-items-center"
+              >
+                <div class="btn-group">
+                  <button
+                    type="button"
+                    class="btn btn-sm btn-outline-secondary"
+                  >
+                    View
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-sm btn-outline-secondary"
+                  >
+                    Edit
+                  </button>
+                </div>
+                <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+              <img src = "https://images-na.ssl-images-amazon.com/images/I/71O3WN6oyzL.jpg"
+              class="bd-placeholder-img card-img-top"
+              width="100%"
+              height="225" />
+            
+            <div class="card-body">
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <div
+                class="d-flex justify-content-between align-items-center"
+              >
+                <div class="btn-group">
+                  <button
+                    type="button"
+                    class="btn btn-sm btn-outline-secondary"
+                  >
+                    View
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-sm btn-outline-secondary"
+                  >
+                    Edit
+                  </button>
+                </div>
+                <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+               <img src = "https://images-na.ssl-images-amazon.com/images/I/71O3WN6oyzL.jpg"
+              class="bd-placeholder-img card-img-top"
+              width="100%"
+              height="225" />
+            
+            <div class="card-body">
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <div
+                class="d-flex justify-content-between align-items-center"
+              >
+                <div class="btn-group">
+                  <button
+                    type="button"
+                    class="btn btn-sm btn-outline-secondary"
+                  >
+                    View
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-sm btn-outline-secondary"
+                  >
+                    Edit
+                  </button>
+                </div>
+                <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mb-4 shadow-sm">
+               <img src = "https://images-na.ssl-images-amazon.com/images/I/71O3WN6oyzL.jpg"
+              class="bd-placeholder-img card-img-top"
+              width="100%"
+              height="225" />
+            
+            <div class="card-body">
+              <p class="card-text">
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </p>
+              <div
+                class="d-flex justify-content-between align-items-center"
+              >
+                <div class="btn-group">
+                  <button
+                    type="button"
+                    class="btn btn-sm btn-outline-secondary"
+                  >
+                    View
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-sm btn-outline-secondary"
+                  >
+                    Edit
+                  </button>
+                </div>
+                <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 )
 export default Images
